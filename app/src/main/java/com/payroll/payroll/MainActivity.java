9861ba801e3895e533cc09fea6a442cb0b5bf018
bbbade <bernadettebumadilla2@gmail.com>
2025-09-23 00:21:06 +0800

    Dashboard CardView Navigations to Payslips, Profile, Attendance, Leave, Deduction, Support/help, Notifications bell

--- app/src/main/java/com/payroll/payroll/MainActivity.java
+++ app/src/main/java/com/payroll/payroll/MainActivity.java
@@ -1,9 +1,13 @@
 package com.payroll.payroll;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -15,10 +19,42 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        CardView cardPayslips = findViewById(R.id.card_payslips);
+        CardView cardProfile  = findViewById(R.id.card_profile);
+        CardView cardAttendance = findViewById(R.id.card_attendance);
+        CardView cardLeave = findViewById(R.id.card_leave);
+        CardView cardDeductions = findViewById(R.id.card_deductions);
+        CardView cardSupport = findViewById(R.id.card_support);
+
+        cardPayslips.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, PayslipsActivity.class);
+            startActivity(intent);
+        });
+
+        cardProfile.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, ProfileActivity.class);
+            startActivity(intent);
+        });
+
+        cardAttendance.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, AttendanceActivity.class);
+            startActivity(intent);
+        });
+
+        cardLeave.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, LeaveActivity.class);
+            startActivity(intent);
+        });
+
+        cardDeductions.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, DeductionsActivity.class);
+            startActivity(intent);
+        });
+
+        cardSupport.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, SupportActivity.class);
+            startActivity(intent);
         });
     }
 }
